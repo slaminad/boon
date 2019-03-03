960a99394a51c07082ebc9308d7cd0dc40222fe9
slaminad <danmerino98@gmail.com>
2019-03-02 21:41:30 -0500

    Refactoring

--- src/main.go
+++ src/main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -19,38 +18,24 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// n, _ := d.AddReport(&db.Report{ID: 0, Header: "Test", Description: "Alert", Author: "Dan",
-	// 	Lat: 51.747169, Lon: -114.333801})
-	// fmt.Println(n)
-
-	d.DeleteReport(1)
-
-	r, _ := d.ListReports()
-	for _, i := range r {
-		fmt.Println(*i)
-	}
-
-	// Close connection to db
-	d.Close()
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 	registerHandlers()
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+
+	d.Close() // Close connection to db
 }
 
 func registerHandlers() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", GetReportsHandler) // GET
-	// r.HandleFunc("/{filter}", FilterGetReportsHandler)    // GET
-	r.HandleFunc("/report/{lat}/{lon}", SetReportHandler) // POST
-
+	r.HandleFunc("/", GetReportsHandler)      // GET
+	r.HandleFunc("/report", SetReportHandler) // POST
 }
 
-// GetReportsHandler - Handles getting reports
+// GetReportsHandler - Load all reports
 func GetReportsHandler(w http.ResponseWriter, r *http.Request) {
 	var reports []db.Report
 
@@ -60,26 +45,19 @@ func GetReportsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// // FilterGetReportsHandler - Filters through reports
-// func FilterGetReportsHandler(w http.ResponseWriter, r *http.Request) {
-// 	var reports []db.Report
-// 	f := mux.Vars(r)["filter"]
-
-// 	filters := strings.Split(f, "-")
-
-// 	allreps, _ := d.ListReports()
-// 	for _, i := range allreps {
-// 		reports = append(reports, *i)
-// 	}
-// }
-
-// SetReportHandler - Handles setting a new report
+// SetReportHandler - Receive a json object and send it to the DB
 func SetReportHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
+	// vars := mux.Vars(r)
 
-	la, _ := strconv.ParseFloat(vars["lat"], 64)
-	lo, _ := strconv.ParseFloat(vars["lon"], 64)
+	// Create report struct and send it to DB handler
+	report := createReportStruct()
 
-	d.AddReport(&db.Report{ID: 0, Header: " ", Description: " ", Author: " ",
-		Lat: la, Lon: lo})
+	d.AddReport(&report)
+}
+
+func createReportStruct() db.Report {
+	// Get JSON object and parse through it
+	r := db.Report{}
+
+	return r
 }
